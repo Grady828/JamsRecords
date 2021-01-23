cadda77d235682bfc093602bb2ea33c6f12c607b
Mike Morris <eight20eight@gmail.com>
2021-01-22 21:16:05 -0500

    Added Bands class and DBContext class

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using JamsRecords.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace JamsRecords
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Bands
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Bandname { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public string IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public string ContactNumber { get; set; }
+    }
+    class JamsRecordsContext : DatabaseContext
+    {
+        public DbSet<Band> Bands { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=JamsRecords");
+        }
 
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+
+        }
+    }
 }
