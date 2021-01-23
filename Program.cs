97f064e29275f1d11da8e40042b8f824be48927e
Mike Morris <eight20eight@gmail.com>
2021-01-22 20:11:26 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JamsRecords.Models;
+using System.Linq;
+
+namespace JamsRecords
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
